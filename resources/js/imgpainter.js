c583f5da4a4ce574521a6a2e7dea6f9e15f7e5d0
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-03-07 15:56:46 -0600

    adds jcanvas

---
+++ resources/js/imgpainter.js
@@ -0,0 +1,13 @@
+$(function() {
+  const $canvas = $('canvas');
+
+  $canvas.drawRect({
+    fillStyle: 'steelblue',
+    strokeStyle: 'blue',
+    strokeWidth: 4,
+    x: 150, y: 100,
+    fromCenter: false,
+    width: 200,
+    height: 100
+  });
+});
