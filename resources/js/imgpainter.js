7cd9b853ce32e88069dcdcdf30ab96904d24e8c8
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-03-08 21:56:30 -0600

    adds images, svgs, and data to shapes.js

--- resources/js/imgpainter.js
+++ resources/js/imgpainter.js
@@ -1,17 +1,23 @@
 window.onload = function() {
   const $canvas = $('canvas');
-  var controls = newControls($canvas);
+  const controls = newControls($canvas);
+  const shape = shapes($canvas);
 
     function resizeCanvas(){
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
       drawStuff();
+      toolActions();
     }
     resizeCanvas();
 
     function drawStuff(){
-      controls.drawShape();
+      // shape.drawShape();
       controls.loadImg();
+    }
+
+    function toolActions(){
+      controls.clearCanvas();
     }
 
 
---
+++ resources/js/shapes.js
@@ -0,0 +1,68 @@
+const shapes = function(canvas){
+
+    // console.log("booyah");
+  //   function drawShape(){
+  //     canvas.drawPolygon({
+  //       layer: true,
+  //       draggable: true,
+  //       bringToFront: true,
+  //       fillStyle: '#c33',
+  //       x: 100, y: 100,
+  //       radius: 50,
+  //       sides: 5,
+  //       concavity: 0.5,
+  //       click: function (layer){
+  //         // Spin star
+  //         $(this).animateLayer(layer, {
+  //           rotate: '+=144'
+  //         });
+  //       }
+  //     });
+  //
+  //     canvas.drawPolygon({
+  //       layer: true,
+  //       draggable: true,
+  //       bringToFront: true,
+  //       fillStyle: '#fff',
+  //       strokeStyle: '#333',
+  //       strokeWidth: 2,
+  //       x: 160, y: 150,
+  //       radius: 100,
+  //       sides: 3,
+  //       mouseover: function(layer) {
+  //         $(this).animateLayer(layer, {
+  //           fillStyle: '#c33',
+  //         }, 500);
+  //       },
+  //       mouseout: function(layer) {
+  //         $(this).animateLayer(layer, {
+  //           fillStyle: '#fff',
+  //         }, 500);
+  //       }
+  //     });
+  //
+  //     canvas.drawArc({
+  //       draggable: true,
+  //       bringToFront: true,
+  //       fillStyle: '#58d',
+  //       shadowColor: '#000',
+  //       shadowBlur: 40,
+  //       shadowX: -15, shadowY: 5,
+  //       x: 50, y: 50,
+  //       radius: 30
+  //     });
+  //
+  //   }
+  //
+  //
+  //
+  //
+  //
+  //
+  // };
+  //
+  //
+  //   return {
+  //     drawShape: drawShape,
+  //   };
+};
